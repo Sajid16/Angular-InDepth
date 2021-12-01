ce69f739ad2d717f40d42df9303608ed1ee39cd4
Sajid16 <sajid.mahboob16@gmail.com>
2021-12-01 18:13:18 +0600

    valuChanges and StatusChanges practice done

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,48 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent implements OnInit {
-
   checkoutForm: FormGroup;
   getUserValues = {
-      "Email": "sajid.mahboob16@gmail.com",
-      "amount": 100,
-      "terms": true
+    Email: 'sajid.mahboob16@gmail.com',
+    amount: 100,
+    terms: true,
   };
+  emailFSub: Subscription;
 
   constructor(private formBuilder: FormBuilder) {
-    
     // this.checkoutForm = this.formBuilder.group({
     //   emailField: new FormControl(),
     //   amountField: new FormControl(),
     //   termField: new FormControl(),
     // });
+
     this.checkoutForm = this.formBuilder.group({
-      emailField: ['', [Validators.minLength(5), Validators.maxLength(25), Validators.required, Validators.email]],
+      emailField: [
+        '',
+        [
+          Validators.minLength(5),
+          Validators.maxLength(25),
+          Validators.required,
+          Validators.email,
+        ],
+      ],
       amountField: ['', Validators.required],
       termField: ['', Validators.requiredTrue],
     });
-  
   }
 
   ngOnInit(): void {
-    // this.checkoutForm.setValue({
-    //   emailField: this.getUserValues.Email,
-    //   amountField: this.getUserValues.amount,
-    //   termField: this.getUserValues.terms
-    // });
+    // valuchanges done inside ngOnInit otherwise it was creating null problem when resetting the form
+    // tracking individual fields chnages
+    this.emailFSub = this.checkoutForm
+      .get('emailField')
+      .valueChanges.subscribe((data) => {
+        console.log(data);
+      });
+
+    // tracking entire form changes
+
+    /* this.emailFSub = this.checkoutForm.valueChanges.subscribe((data) => {
+      console.log(data);
+    }); */
 
     // this.setValueFunc();
-    // this.checkoutForm.patchValue({
-    //   emailField: this.getUserValues.Email,
-    //   // amountField: this.getUserValues.amount,
-    //   termField: this.getUserValues.terms
-    // });
   }
 
   get emailField() {
@@ -53,29 +69,31 @@ export class ReactiveFormComponent implements OnInit {
     return this.checkoutForm.get('amountField');
   }
 
-  checkoutPost(){
+  checkoutPost() {
     console.log(this.checkoutForm.value);
+    if (this.emailFSub) {
+      this.emailFSub.unsubscribe();
+    }
     this.resetForm();
   }
 
-  resetForm(){
+  resetForm() {
     this.checkoutForm.reset();
   }
 
-  setValueFunc(){
+  setValueFunc() {
     this.checkoutForm.setValue({
       emailField: this.getUserValues.Email,
       amountField: this.getUserValues.amount,
-      termField: this.getUserValues.terms
+      termField: this.getUserValues.terms,
     });
   }
 
-  patchValueFunc(){
+  patchValueFunc() {
     this.checkoutForm.patchValue({
       emailField: this.getUserValues.Email,
       // amountField: this.getUserValues.amount,
-      termField: this.getUserValues.terms
+      termField: this.getUserValues.terms,
     });
   }
-
 }
